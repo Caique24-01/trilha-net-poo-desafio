a807ae90a645af853abc29186eab4dcce7d18264
Caique24-01 <caiqueleme100@gmail.com>
2024-08-28 16:53:51 -0300

    Desafio Trilha Net POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("(11)940028922", "Iphone 16", "23443759603945", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+iphone.ReceberLigacao();
+Console.WriteLine("");
+Nokia nokia = new Nokia("(11)997650394", "Nokia G60 5G", "2334567830495823", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Shopee");
+nokia.ReceberLigacao();
